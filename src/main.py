7e03b5a89eda104202870f6f6f640cc051275d4e
MudiLoodi <wadi.38@hotmail.com>
2024-02-15 18:45:44 +0100

    Added blink detection.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import cv2
 import dlib
+from math import hypot
 
 # Load Dlib's pre-trained face and landmark detector
 face_detector = dlib.get_frontal_face_detector()
@@ -20,8 +21,29 @@ def detect_pupil(vid):
 
     for face in faces:
         landmarks = landmark_predictor(gray_image, face)
-        left_point = (landmarks.part(36).x, landmarks.part(36).y)
-        right_point = (landmarks.part(39).x, landmarks.part(39).y)
+        
+        left_eye_ratio = detect_blink(vid, landmarks, [36, 37, 38, 39, 40, 41])
+        right_eye_ratio = detect_blink(vid, landmarks, [42, 43, 44, 45, 46, 47])
+        blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
+        
+        if blinking_ratio > 5.6:
+            cv2.putText(vid, "Blinking", (50,150), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0))
+
+def detect_blink(vid, eye_landmarks, eye_points):
+    left_point = (eye_landmarks.part(eye_points[0]).x, eye_landmarks.part(eye_points[0]).y)
+    right_point = (eye_landmarks.part(eye_points[3]).x, eye_landmarks.part(eye_points[3]).y)
+    center_top = midpoint(eye_landmarks.part(eye_points[1]), eye_landmarks.part(eye_points[2]))
+    center_bottom = midpoint(eye_landmarks.part(eye_points[5]), eye_landmarks.part(eye_points[4]))
+    # circle = cv2.circle(vid, (center_top), 3, (0, 0, 255), 2)
+    
+    hor_line  = cv2.line(vid, left_point, right_point, (0,255,0), 2)
+    ver_line = cv2.line(vid, center_top, center_bottom, (0,255,0), 2)
+
+    hor_line_length = hypot((left_point[0]-right_point[0]), (left_point[1]-right_point[1]))
+    ver_line_length = hypot((center_top[0]-center_bottom[0]), (center_top[1]-center_bottom[1]))
+
+    ratio = hor_line_length/ver_line_length
+    return ratio
 
 # Main loop for eye tracking using calibration mapping
 while True:
